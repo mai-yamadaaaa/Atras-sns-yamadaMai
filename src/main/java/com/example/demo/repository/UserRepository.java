56310c592fd9054398abe1260a596aa269b3fef0
山田麻衣 <yamad425@gmail.com>
2025-07-06 18:17:48 +0900

    ユーザ検索機能

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.User;
@@ -7,4 +9,6 @@ import com.example.demo.entity.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	User findByEmailAndPassword(String email, String password);
+
+	List<User> findByUsernameContaning(String keyword);
 }
